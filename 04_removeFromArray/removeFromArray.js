5298a0af1060dd7c1bd31c565b844bd24e5cf41a
Loneastro95 <ayandasontlaba6@gmail.com>
2024-07-12 12:10:31 +0200

    Had to remove a value from an array as given in the test case

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, value) {
+   let newArray = [];
+   for(let i = 0; i< arr.length; i++){
+    if(arr[i] !== value){
+        newArray.push(arr[i]);
+    }
+   }
+   return newArray;
 };
 
 // Do not edit below this line
