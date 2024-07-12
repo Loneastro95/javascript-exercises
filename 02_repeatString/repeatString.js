14debbfb965ee8b914cef7eac4c50fb5c6e34933
Loneastro95 <ayandasontlaba6@gmail.com>
2024-07-12 11:42:57 +0200

    Second exercise where i need to repeat string using the test case for reference using loop

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, number) {
+    let repeatString = "";
+//  while(number > 0){
+//    repeatString += string;
+//    number --;
+//  }
+for(repeatString; number > 0; number --){
+    repeatString += string;
+}
+ return repeatString;
 };
 
 // Do not edit below this line
