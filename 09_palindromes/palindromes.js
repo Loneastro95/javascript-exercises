9c699af1473a3eb47d6773e0c12d2bcb2e3edea1
Loneastro95 <ayandasontlaba6@gmail.com>
2024-07-29 15:03:24 +0200

    getTitles of book and fibonacci

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  let nopunctuation = str.replace(/\W/g,"");
+  let nospaces = nopunctuation.replace(/\s/g,"");
+  let finalstring = nospaces.toLowerCase();
 
-};
+  let len = finalstring.length;
+  for ( let i = 0; i < Math.floor(len/2); i++ ) {
+    if (finalstring[i] !== finalstring[len - 1 - i]) {
+      return false;
+    }
+  }
+  return true;    
+}
 
 // Do not edit below this line
 module.exports = palindromes;
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+  // let arr = [0,1]
+  // for(let i =2; i< n + 1; i++){
+  //     arr.push(arr[i-2] + arr[i-1])
+  // }
+  // return arr[n]
 
+  // if(n < 2){
+  //     return n
+  // }
+  // return fibonacci(n-1) + fibonacci(n -2)
+
+  if (n == 0) {
+    return 0;
+  }
+  if (n == 1) {
+    return 1;
+  } else {
+    return fibonacci(n - 1) + fibonacci(n - 2);
+  }
 };
 
 // Do not edit below this line
